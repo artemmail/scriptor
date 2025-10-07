07d08519993babaacb959b0901b82d51017bf091
artemmail <artemmail@gmail.com>
2025-10-07 20:22:20 +0300

    Fix image editor fullscreen layout

--- Angular/youtube-downloader/src/app/png-to-webp/png-to-webp.component.ts
+++ Angular/youtube-downloader/src/app/png-to-webp/png-to-webp.component.ts
@@ -213,8 +213,8 @@ export class PngToWebpComponent implements OnDestroy {
         name: this.originalFile()?.name ?? 'image.png',
       },
       panelClass: 'image-editor-panel',
-      width: '90vw',
-      maxWidth: '960px',
+      width: '100%',
+      maxWidth: 'calc(100vw - 48px)',
     });
 
     const result = await firstValueFrom(dialogRef.afterClosed());
