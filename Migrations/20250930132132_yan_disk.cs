fb576f1b50fec78a993c9953b29e53c490b79f4d
ruticker1 <ruticker@gmail.com>
2025-10-10 15:56:11 +0300

    lost migrations

---
+++ Migrations/20250930132132_yan_disk.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace YandexSpeech.Migrations
+{
+    /// <inheritdoc />
+    public partial class yan_disk : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
