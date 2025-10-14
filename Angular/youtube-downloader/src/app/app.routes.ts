702f8f8671a8e8b965b184e80026acd4333691b7
artemmail <artemmail@gmail.com>
2025-10-14 19:06:28 +0300

    Improve SSR configuration and browser-only guards

--- Angular/youtube-downloader/src/app/app.routes.ts
+++ Angular/youtube-downloader/src/app/app.routes.ts
@@ -14,15 +14,57 @@ import { About3Component } from './about3/about3.component';
 import { RoleGuard } from './services/role.guard';
 import { AdminUsersComponent } from './admin-users/admin-users.component';
 import { AdminRecognitionProfilesComponent } from './admin-recognition-profiles/admin-recognition-profiles.component';
+import { RecognitionControlComponent } from './app-recognition-control/recognition-control.component';
+import { TaskPageComponent } from './task-page/task-page.component';
+import { ServiceNewsComponent } from './service-news/service-news.component';
+import { AudioFilesComponent } from './audio-file/audio-files.component';
+import { AuthCallbackComponent } from './AuthCallbackComponent/auth-callback.component';
+import { LoginComponent } from './LoginComponent/login.component';
+import { EditorPageComponent } from './editor-pade/editor-page.component';
+import { BlogFeedComponent } from './blog-feed/blog-feed.component';
+import { BlogTopicCreateComponent } from './blog-topic-create/blog-topic-create.component';
+import { BlogTopicDetailComponent } from './blog-topic-detail/blog-topic-detail.component';
+import { ProfileComponent } from './profile/profile.component';
+import { TranscriptionEditorComponent } from './transcription-editor/transcription-editor.component';
 
 export const appRoutes: Routes = [
-  { path: '', component: OpenAiTranscriptionComponent },
-  { path: 'youtube-downloader', component: YoutubeDownloaderComponent },
+  { path: 'auth/callback', component: AuthCallbackComponent },
+  { path: 'audio', component: AudioFilesComponent },
+  { path: 'transcriptions', component: OpenAiTranscriptionComponent, canActivate: [AuthGuard] },
+  { path: 'transcriptions/:id/edit', component: TranscriptionEditorComponent, canActivate: [AuthGuard] },
+  { path: 'about', component: AboutBusinessComponent },
+  { path: 'about1', component: About1Component },
+  { path: 'about2', component: About2Component },
+  { path: 'about3', component: About3Component },
   { path: 'down', component: YoutubeDownloaderComponent },
-  { path: 'recognition-tasks', component: SubtitlesTasksComponent },
-  { path: 'transcriptions', component: OpenAiTranscriptionComponent },
+  { path: 'youtube-downloader', component: YoutubeDownloaderComponent },
+  { path: '', component: About3Component },
+  { path: 'recognition', component: RecognitionControlComponent },
+  { path: 'recognition-tasks', component: RecognitionTasksComponent },
+  { path: 'y', component: YoutubeDownloaderComponent },
+  { path: 'recognized/:id', component: TaskPageComponent },
+  { path: 'markdown-converter/:id', component: MarkdownConverterComponent },
   { path: 'markdown-converter', component: MarkdownConverterComponent },
   { path: 'png-to-webp', component: PngToWebpComponent },
+  { path: 'edit/:id', component: EditorPageComponent },
+  { path: 'ServiceNews/Content/:id', component: TaskPageComponent },
+  { path: 'tasks', component: SubtitlesTasksComponent },
+  { path: 'Scriptorium', component: SubtitlesTasksComponent },
+  {
+    path: 'blog/new',
+    component: BlogTopicCreateComponent,
+    canActivate: [RoleGuard],
+    data: { roles: ['Moderator'] }
+  },
+  {
+    path: 'blog/:slug/edit',
+    component: BlogTopicCreateComponent,
+    canActivate: [RoleGuard],
+    data: { roles: ['Moderator'] }
+  },
+  { path: 'blog/:slug', component: BlogTopicDetailComponent },
+  { path: 'ServiceNews', component: ServiceNewsComponent },
+  { path: 'blog', component: BlogFeedComponent },
   { path: 'billing', component: BillingComponent, canActivate: [AuthGuard] },
   {
     path: 'admin/users',
@@ -36,9 +78,7 @@ export const appRoutes: Routes = [
     canActivate: [RoleGuard],
     data: { roles: ['Admin'] }
   },
-  { path: 'about', component: AboutBusinessComponent },
-  { path: 'about1', component: About1Component },
-  { path: 'about2', component: About2Component },
-  { path: 'about3', component: About3Component },
-
+  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
