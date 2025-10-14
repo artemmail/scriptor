70102cfb6474d296a637fff29eb77eb2b147e2dd
artemmail <artemmail@gmail.com>
2025-10-14 19:33:41 +0300

    Fix SSR build to start Express server

--- Angular/youtube-downloader/src/main.server.ts
+++ Angular/youtube-downloader/src/main.server.ts
@@ -1,7 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
+import { startServer } from './server';
 
 const bootstrap = () => bootstrapApplication(AppComponent, config);
+
+startServer();
 
 export default bootstrap;
