1808b3c322322ab32aa0bd8bbba00bfe88b461a2
ruticker1 <ruticker@gmail.com>
2026-03-09 13:49:55 +0300

    бд

---
+++ Migrations/20260309085946_AddTitleToOpenAiTranscriptionTasks.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace YandexSpeech.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTitleToOpenAiTranscriptionTasks : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "OpenAiTranscriptionTasks",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "OpenAiTranscriptionTasks");
+        }
+    }
+}
